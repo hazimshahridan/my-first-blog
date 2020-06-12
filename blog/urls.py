89cb795380fc5fc4062bbd3d18825843a2f34c63
hazimshahridan <hazimshahridan@gmail.com>
2020-06-12 16:17:45 +0800

    Added multi page, edited html, added basic css elements, rev5

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('about',views.webpage2, name='webpage2'),
+    path('contact',views.webpage3, name='webpage3'),
+
 ]
