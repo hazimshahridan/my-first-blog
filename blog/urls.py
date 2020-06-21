0fc52f302f8182cacafeec5ff93d31418a5f8947
hazimshahridan <hazimshahridan@gmail.com>
2020-06-21 10:52:21 +0800

    added more html css, taskbar, edit and create blog psots, multi page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
+
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('about',views.webpage2, name='webpage2'),
     path('contact',views.webpage3, name='webpage3'),
-
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
